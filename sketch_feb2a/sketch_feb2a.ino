cb0578453cd7de102963df80b8b58c7d72e6a9dc
Mr-Hansen-Orewa <d.hansen@orewacollege.nz>
2024-02-08 14:52:16 +1300

    Update sketch_feb2a.ino
    testing stuff with the class

--- sketch_feb2a/sketch_feb2a.ino
+++ sketch_feb2a/sketch_feb2a.ino
@@ -1,9 +1,46 @@
-void setup() {
-  // put your setup code here, to run once:
+const byte LEDPIN = 12;
+const byte PAWPIN1 = 1;
+const byte PAWPIN2 = 2;
+const byte PAWPIN3 = 3;
+const byte PAWPIN4 = 4;
 
+void setup() {
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(PAWPIN1, INPUT);
+  pinMode(PAWPIN2, INPUT);
+  pinMode(PAWPIN3, INPUT);
+  pinMode(PAWPIN4, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // If paw 1 is pressed then do the following
+  if (digitalRead(PAWPIN1) == HIGH) {
+    digitalWrite(LEDPIN, HIGH);
+  }
+  // If paw 2 is pressed then do the following
+  if (digitalRead(PAWPIN2) == HIGH) {
+    digitalWrite(LEDPIN, LOW);
+  }
+  // If paw 3 is pressed then do the following
+  if (digitalRead(PAWPIN3) == HIGH) {
+    playMusic();
+  }
+  // If paw 4 is pressed then do the following
+  if (digitalRead(PAWPIN4) == HIGH) {
+    int score = playGame();
+  }
 }
+
+void playMusic(){
+  //play an MP3 file here
+}
+
+int playGame(){
+  byte score = 99;
+  //play simon says game
+  return score;
+}
+
+
+
+
